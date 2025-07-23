602878ab8bf82b0a0f157e593180fd7388d80410
roselee <2680696596@qq.com>
2025-07-23 20:48:36 +0800

    update

--- src/main/java/com/roselee/softwaretest/Application.java
+++ src/main/java/com/roselee/softwaretest/Application.java
@@ -1,12 +1,11 @@
 package com.roselee.softwaretest;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author RoseLee
  * @date 2025/7/23 20:35
  */
-@SpringBootApplication
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
